2be7f3e532526f3d8acd237c69ad5a430dfd9b13
Martin Bukatovic <martin.bukatovic@gmail.com>
2021-08-28 15:04:09 +0200

    ThreeColumns and CenteredMaster layout
    Use ThreeColumns and CenteredMaster layout configuration, dropping
    unused layout imports.

--- xmonad.hs
+++ xmonad.hs
@@ -15,12 +15,11 @@ import XMonad.Config.Gnome
 import XMonad.Config.Kde
 import XMonad.Config.Xfce
 
-import XMonad.Layout.NoBorders
-import XMonad.Layout.IM
-import XMonad.Layout.SimpleFloat
+import XMonad.Layout.CenteredMaster
 import XMonad.Layout.Grid
-import XMonad.Layout.Tabbed
+import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.ThreeColumns
 
 import XMonad.Actions.PhysicalScreens
 import XMonad.Actions.CycleWS
@@ -130,16 +129,18 @@ myStartupHook = setWMName "LG3D"
 
 myLayoutHook = smartBorders $ avoidStruts $ coreLayoutHook
 
-coreLayoutHook = tiled ||| Mirror tiled ||| Full ||| Grid
+coreLayoutHook = tiled ||| Mirror tiled ||| tiled3 ||| Full ||| Grid ||| centerMaster Grid
   where
     -- default tiling algorithm partitions the screen into two panes
-    tiled   =  ResizableTall nmaster delta ratio []
+    tiled   = ResizableTall nmaster delta ratio []
+    -- like the tall, but with three columns
+    tiled3  = ThreeColMid nmaster delta ratio
     -- The default number of windows in the master pane
     nmaster = 1
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1/2
     -- Percent of screen to increment by when resizing panes
     delta   = 3/100
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
 
 --
 -- log hook (for xmobar)
